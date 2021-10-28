cb3d408d5a4a4d41ee7f9524bded879caea3a90b
l975421700 <gaoqg229@gmail.com>
2021-10-28 13:36:13 +0100

    20211028

--- c_python/00_preliminary_analysis/00.00_plot_study_area.py
+++ c_python/00_preliminary_analysis/00.00_plot_study_area.py
@@ -14,6 +14,7 @@ import sys  # print(sys.path)
 sys.path.append('/home/users/qino')
 
 # plot
+import matplotlib.path as mpath
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
@@ -76,4 +77,52 @@ fig.savefig('figures/00_test/natural_earth.png', dpi=1200)
 # endregion
 # =============================================================================
 
+
+# =============================================================================
+# region plot Antarctica
+
+fig, ax = plt.subplots(
+    1, 1, figsize=np.array([8.8, 8]) / 2.54, subplot_kw={'projection': ccrs.SouthPolarStereo()}, dpi=600)
+
+ax.set_extent((-180, 180, -90, -60), crs=ccrs.PlateCarree())
+
+coastline = cfeature.NaturalEarthFeature(
+    'physical', 'coastline', '10m', edgecolor='black',
+    facecolor='none', lw=0.25)
+ax.add_feature(coastline, zorder=2)
+borders = cfeature.NaturalEarthFeature(
+    'cultural', 'admin_0_boundary_lines_land', '10m',
+    edgecolor='black',
+    facecolor='none', lw=0.25)
+ax.add_feature(borders, zorder=2)
+
+ax.gridlines(crs=ccrs.PlateCarree(), linewidth=0.25, zorder=2,
+             color='gray', alpha=0.5, linestyle='--')
+
+# Compute a circle in axes coordinates, which we can use as a boundary
+# for the map. We can pan/zoom as much as we like - the boundary will be
+# permanently circular.
+theta = np.linspace(0, 2*np.pi, 100)
+center, radius = [0.5, 0.5], 0.5
+verts = np.vstack([np.sin(theta), np.cos(theta)]).T
+circle = mpath.Path(verts * radius + center)
+ax.set_boundary(circle, transform=ax.transAxes)
+
+figure_margin = {
+    'left': 0.12, 'right': 0.96, 'bottom': 0.05, 'top': 0.995}
+fig.subplots_adjust(
+    left=figure_margin['left'], right=figure_margin['right'],
+    bottom=figure_margin['bottom'], top=figure_margin['top'])
+fig.savefig('figures/00_test/00_trial.png')
+
+
+'''
+# https://scitools.org.uk/cartopy/docs/latest/gallery/lines_and_polygons/always_circular_stereo.html#sphx-glr-gallery-lines-and-polygons-always-circular-stereo-py
+
+ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.OCEAN)
+
+'''
+# endregion
+# =============================================================================
 
