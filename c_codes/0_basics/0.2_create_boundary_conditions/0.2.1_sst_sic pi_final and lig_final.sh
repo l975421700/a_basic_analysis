0caafb6d10bd30fe95ba30865398e9d58861034c
Qinggang Gao <a270200@levante2.atos.local>
2022-08-05 16:34:22 +0200

    create bc from lgm-50 on levante

--- c_codes/0_basics/0.2_create_boundary_conditions/0.2.1_sst_sic pi_final and lig_final.sh
+++ c_codes/0_basics/0.2_create_boundary_conditions/0.2.1_sst_sic pi_final and lig_final.sh
@@ -8,6 +8,7 @@ source ${HOME}/miniconda3/bin/activate deepice
 #-------- create ensemble mean
 
 ls /home/a/a270064/bb1029/awiesm_exp/lig_final/outdata/fesom/sst*
+# 2401 - 3001
 
 cdo -ensmean /home/a/a270064/bb1029/awiesm_exp/lig_final/outdata/fesom/sst.fesom.29??01.01.nc /work/ab0246/a270200/startdump/model_input/lig/bc_lig_pi_final/sst.fesom.2900_2999.lig_final.nc
 
@@ -17,6 +18,7 @@ cdo -f nc copy -timmean -chname,var72,wisosw_d -select,code=72 /home/a/a270064/b
 
 
 #-------- regrid and change attrs
+# 2072 - 3100
 
 cdo -setattribute,sst@units=K -addc,273.15 -remapcon,T63grid -setgrid,/work/ab0246/a270200/startdump/fesom2/mesh/mesh_CORE2_finaltopo_mean/sl.grid.CDO /work/ab0246/a270200/startdump/model_input/lig/bc_lig_pi_final/sst.fesom.2900_2999.lig_final.nc /work/ab0246/a270200/startdump/model_input/lig/bc_lig_pi_final/sst.fesom.2900_2999.lig_final_t63.nc
 
@@ -47,7 +49,23 @@ cdo -chname,a_ice,sic -mulc,100 -remapcon,T63grid -setgrid,/work/ab0246/a270200/
 
 #-------- create ensemble mean
 
-# ls /home/a/a270064/bb1029/awiesm_exp/pi_final/outdata/fesom/sst*
+ls /home/a/a270064/bb1029/AWIESM2.1-EXP/lgm-50/outdata/fesom/sst*
+# 2000-2233
+
+cdo -ensmean /home/a/a270064/bb1029/AWIESM2.1-EXP/lgm-50/outdata/fesom/sst.fesom.22[0-2]?01.01.nc /work/ab0246/a270200/startdump/model_input/lgm/lgm-50/sst.fesom.2200_2229.lgm-50.nc
+
+cdo -ensmean /home/a/a270064/bb1029/AWIESM2.1-EXP/lgm-50/outdata/fesom/a_ice.fesom.22[0-2]?01.01.nc /work/ab0246/a270200/startdump/model_input/lgm/lgm-50/sic.fesom.2200_2229.lgm-50.nc
+
+cdo -f nc copy -timmean -chname,var72,wisosw_d -select,code=72 /home/a/a270064/bb1029/AWIESM2.1-EXP/lgm-50/outdata/echam/MM_lgm-50_22[0-2]???.01_wiso /work/ab0246/a270200/startdump/model_input/lgm/lgm-50/wisosw_d.echam.2200_2229.lgm-50.nc
+
+#-------- regrid and change attrs
+
+
+cdo -setattribute,sst@units=K -addc,273.15 -remapdis,T63grid -setgrid,/work/ab0246/a270200/startdump/model_input/lgm/lgm-50/mesh_description_lgm.txt /work/ab0246/a270200/startdump/model_input/lgm/lgm-50/sst.fesom.2200_2229.lgm-50.nc /work/ab0246/a270200/startdump/model_input/lgm/lgm-50/sst.fesom.2200_2229.lgm-50_t63.nc
+
+cdo -chname,a_ice,sic -mulc,100 -remapdis,T63grid -setgrid,/work/ab0246/a270200/startdump/model_input/lgm/lgm-50/mesh_description_lgm.txt /work/ab0246/a270200/startdump/model_input/lgm/lgm-50/sic.fesom.2200_2229.lgm-50.nc /work/ab0246/a270200/startdump/model_input/lgm/lgm-50/sic.fesom.2200_2229.lgm-50_t63.nc
+
+
 
 
 
